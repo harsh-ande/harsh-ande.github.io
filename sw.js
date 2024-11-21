60981bde114956229f7f4cb94f171d67ec576d6f
Harsh Vardhan <harsh.vardhan.ande@gmail.com>
2024-11-20 21:14:17 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c44eb88d9bb5798c9500.js"
+    "url": "webpack-runtime-5ce1c140e77dedb23c66.js"
   },
   {
     "url": "framework-1b1ee3e281fdc8e00d36.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "aacf5718454549664073fc7b3cbd5f75"
+    "revision": "29df0173865e686956b39bbac1fe6519"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-162d54bec23a104748fe.js"
